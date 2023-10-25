b0003f4cdba1d766312c702e5410ce1fa311b076
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-10-25 09:23:56 +0500

    Add project files.

---
+++ ADO.NET/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+
+using (SqlConnection connect = new SqlConnection())
+{
+    connect.ConnectionString = "Server = (localdb)\\MSSQLLocalDB;;Database=Lms; Trusted_Connection = true;";
+
+    connect.Open();
+
+    string query = $"Insert into Course(id,name,TeacherId,SubjectId) values(1,'Sarvar',3,5)";
+
+    SqlCommand sqlCommand = new SqlCommand(query, connect);
+
+    using(SqlDataReader reader = sqlCommand.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            Console.WriteLine($"ID: {reader[0]}");
+
+            Console.WriteLine($"Name: {reader["Name"]}");
+        }
+    
+    }
+}
